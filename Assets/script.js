2b76a5e123c711e526c407a3a5a86d9670feb422
Anthony Davis <Anthony_Davis89@icloud.comgit config  --global user.email git config  --global user.email Anthony_Davis89@icloud.comgit config  --global user.email Anthony_Davis89@icloud.com>
2021-03-27 18:04:01 -0400

    Fixing center

--- Assets/script.js
+++ Assets/script.js
@@ -11,23 +11,65 @@ let weatherIcon = $(".weatherIcon");
 let searchHistory = $(".historyItems");
 
 // Main area
-let cityTemp = $(".city-temp");
-let cityHum = $(".city-hum");
-let cityWind = $(".city-wind");
-let cityUV = $(".city-UV");
+// let cityTemp = $(".city-temp");
+// let cityHum = $(".city-hum");
+// let cityWind = $(".city-wind");
+// let cityUV = $(".city-UV");
 
 // 5-Day Forecast
 
 //
 searchBtn.on("click", function () {
   console.log("clicked button");
-  console.log(searchInput.val());
-  // getWeather(searchInput.val());
+  //   console.log(searchInput.val());
+
+  var city = searchInput.val();
+  getWeather(city);
 });
+
+function getWeather(city) {
+  console.log("Get Weather Function");
+  console.log(city);
+  let queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`;
+  $.ajax({
+    url: queryUrl,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    var cityTemp = Math.round(response.main.temp);
+    var cityHum = response.main.humidity;
+    var cityWind = response.wind.speed;
+    var cityLon = response.coord.lon;
+    var cityLat = response.coord.lat;
+    var weatherIcon = response.weather[0].icon;
+    var iconURL = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x" + ".png";
+    
+
+   
+
+    $(".city-heading")
+      .html(
+        "<h1>" +
+          searchInput.val() +
+          " " +
+          "(" +
+          date +
+          ")" +
+          "</h1>"
+      )
+      .addClass("city-heading");
+
+    $(".city-temp").html("<p>Temperature: " + cityTemp +  "</p>");
+    $(".city-hum").html("<p>Humidity: " + cityHum +  "</p>");
+    $(".city-wind").html("<p>Wind Speed: " + cityWind +  "</p>");
+    $("img").attr("src", iconURL);
+  });
+}
+
 
 //
 $(".city-heading").append(" " + date);
-$(".city-heading span").text(city);
+
 
 // function getWeather(cityInput) {
 //     let queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputy}&APPID=${apiKey}&units=imperial`;
