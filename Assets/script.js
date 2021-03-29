4992f1a82f7e150589d1992f958ffc1310e74b02
Anthony Davis <Anthony_Davis89@icloud.comgit config  --global user.email git config  --global user.email Anthony_Davis89@icloud.comgit config  --global user.email Anthony_Davis89@icloud.com>
2021-03-29 13:27:46 -0400

    5-day forecast

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,8 @@ let apiKey = "d3591e8d8114fff09831e57f3d366828";
 var searchBtn = $(".btn-test");
 var searchInput = $(".searchInput");
 let historyItem = $(".history-item");
-var date = moment().format("l");
+var date = moment().format("l")
+var cityHistoryArray = [];
 
 // Left column
 let cityName = $(".city-heading");
@@ -16,12 +17,13 @@ let searchHistory = $(".historyItems");
 //
 searchBtn.on("click", function () {
   console.log("clicked button");
+  // if(cityHistoryArray == null) {
+  //   setStorage()  
+  // } else {
+
+  // }
+  setStorage(); 
   //   Local Storage 
-  var position = 0;
-  var inputStorage = searchInput.val();
-  localStorage.setItem(position, inputStorage);
-  position ++;
-  console.log("push my buttons pls"); 
 
   var city = searchInput.val();
   getWeather(city);
@@ -45,13 +47,71 @@ function getWeather(city) {
     
 
 
-  let queryUrl2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude={part}&APPid=${apiKey}`;
+  let queryUrl2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude={part}&units=imperial&APPid=${apiKey}`;
+
   $.ajax({
     url: queryUrl2,
     method: "GET",
   }).then(function (responseAll) {
     console.log(responseAll);
     var cityUiv = responseAll.current.uvi;
+    var weatherIcon5day0 = responseAll.daily[0].weather[0].icon;
+    var weatherIcon5day1 = responseAll.daily[1].weather[0].icon;
+    var weatherIcon5day2 = responseAll.daily[2].weather[0].icon;
+    var weatherIcon5day3 = responseAll.daily[3].weather[0].icon;
+    var weatherIcon5day4 = responseAll.daily[4].weather[0].icon;
+    var iconURL5day0 = "http://openweathermap.org/img/wn/" + weatherIcon5day0 + "@2x" + ".png";
+    var iconURL5day1 = "http://openweathermap.org/img/wn/" + weatherIcon5day1 + "@2x" + ".png";
+    var iconURL5day2 = "http://openweathermap.org/img/wn/" + weatherIcon5day2 + "@2x" + ".png";
+    var iconURL5day3 = "http://openweathermap.org/img/wn/" + weatherIcon5day3 + "@2x" + ".png";
+    var iconURL5day4 = "http://openweathermap.org/img/wn/" + weatherIcon5day4 + "@2x" + ".png";
+
+
+console.log(iconURL5day0)
+console.log(iconURL5day1)
+console.log(iconURL5day2)
+console.log(iconURL5day3)
+  
+      
+
+      var temp0Hold = (Math.round(responseAll.daily[0].temp.day));
+      var temp1Hold = (Math.round(responseAll.daily[1].temp.day));
+      var temp2Hold = (Math.round(responseAll.daily[2].temp.day));
+      var temp3Hold = (Math.round(responseAll.daily[3].temp.day));
+      var temp4Hold = (Math.round(responseAll.daily[4].temp.day));
+
+      $(".card-temp0").html("<p>Temperature: " + temp0Hold +  "</p>");
+      $(".card-temp1").html("<p>Temperature: " + temp1Hold +  "</p>");
+      $(".card-temp2").html("<p>Temperature: " + temp2Hold +  "</p>");
+      $(".card-temp3").html("<p>Temperature: " + temp3Hold +  "</p>");
+      $(".card-temp4").html("<p>Temperature: " + temp4Hold +  "</p>");
+    
+
+      $(".card-Hum0").append(responseAll.daily[0].humidity);
+      $(".card-Hum1").append(responseAll.daily[1].humidity);
+      $(".card-Hum2").append(responseAll.daily[2].humidity);
+      $(".card-Hum3").append(responseAll.daily[3].humidity);
+      $(".card-Hum4").append(responseAll.daily[4].humidity);
+
+      $("card-image0").attr("src", iconURL5day0);
+      console.log(iconURL5day0)
+      $("card-image1").attr("src", iconURL5day1);
+      $("card-image2").attr("src", iconURL5day2);
+      $("card-image3").attr("src", iconURL5day3);
+      $("card-image4").attr("src", iconURL5day4);
+
+      var date1 = moment().add(1, 'days').calendar(); 
+      var date2 = moment().add(2, 'days').calendar(); 
+      var date3 = moment().add(3, 'days').calendar(); 
+      var date4 = moment().add(4, 'days').calendar(); 
+      var date5 = moment().add(5, 'days').calendar(); 
+
+      $(".date1").html("<h5>" + date1+  "</h5>");
+      $(".date2").html("<h5>" + date2+  "</h5>");
+      $(".date3").html("<h5>" + date3+  "</h5>");
+      $(".date4").html("<h5>" + date4+  "</h5>");
+      $(".date5").html("<h5>" + date5+  "</h5>");
+
 
     $(".city-heading")
     .html(
@@ -80,6 +140,24 @@ function getWeather(city) {
 
 //
 $(".city-heading").append(" " + date);
+
+function setStorage() {
+  localStorage.setItem("localCityHistory", JSON.stringify(searchInput.val()));
+  onload();
+}
+
+function onload() {
+//  cityHistoryArray.html("");
+if(localStorage.getItem("localCityHistory")) {
+  cityHistoryArray = JSON.parse(localStorage.getItem("localCityHistory"));
+
+  for (let index = 0; index < cityHistoryArray.length; index++) {
+    // $("#[index]").append(cityHistoryArray[index])
+    
+  }
+}
+
+}
 
 // function getSchedule() {
 //   var positionStorage = $('.row').toArray();
